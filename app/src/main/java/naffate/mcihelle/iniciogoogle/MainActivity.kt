92304aa9b7dfe18418d79526fc0995fef9c80fe5
Michelle Naffate <michelle.naffate205164@potros.itson.edu.mx>
2021-04-24 19:55:44 -0700

    Práctica 10 - Inicio de sesión con Google

---
+++ app/src/main/java/naffate/mcihelle/iniciogoogle/MainActivity.kt
@@ -0,0 +1,105 @@
+package naffate.mcihelle.iniciogoogle
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+    val RC_SIGN_IN = 343
+    val LOG_OUT = 234
+    lateinit var mGoogleSignInClient: GoogleSignInClient
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        sign_in_button.setSize(SignInButton.SIZE_WIDE)
+
+        sign_in_button.setOnClickListener{
+            val signInIntent = mGoogleSignInClient.signInIntent
+            startActivityForResult(signInIntent, RC_SIGN_IN)
+
+        }
+        // Configure sign-in to request the user's ID, email address, and basic
+// profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        // Configure sign-in to request the user's ID, email address, and basic
+// profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        val account = GoogleSignIn.getLastSignedInAccount(this)
+        updateUI(account)
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN){
+            val task =
+                GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInResult(task)
+
+        }
+
+        if(requestCode == LOG_OUT){
+            signOut()
+        }
+    }
+
+    private fun signOut() {
+        mGoogleSignInClient.signOut()
+            .addOnCompleteListener(this, OnCompleteListener<Void?> {
+                Toast.makeText(this, "Sesión terminada", Toast.LENGTH_SHORT).show()
+            })
+    }
+
+    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account =
+                completedTask.getResult(ApiException::class.java)
+
+            updateUI(account)
+        } catch (e: ApiException) {
+            Log.w("test_signin", "signInResult:failed code=" + e.statusCode)
+            updateUI(null)
+        }
+    }
+
+    private fun updateUI(acct: GoogleSignInAccount?) {
+
+        if (acct != null){
+            val intent = Intent(this, PrincipalActivity::class.java)
+
+            intent.putExtra("name", acct.displayName)
+            intent.putExtra("email", acct.email)
+            startActivity(intent)
+            startActivityForResult(intent, LOG_OUT)
+        }
+    }
+
+
+
+
+}
